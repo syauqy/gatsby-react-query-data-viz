ef464243df2bc6ec6f17d177b6bc8055bdfb3d3e
Jala Tech Pter. Ltd <jalaengineering@Jalas-iMac.local>
2020-11-14 20:43:42 +0700

    add airtable data query

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+console.log(`Using environment config: '${activeEnv}'`);
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   /* Your site config here */
   plugins: [],
--- src/pages/app.js
+++ src/pages/app.js
@@ -11,16 +11,18 @@ export default function App() {
     )
 }
 
-const fetchExchange = async (currency) => {
-    const response = await fetch(`https://api.ratesapi.io/api/latest?base=${currency}`);
+const fetchExchange = async (table) => {
+    // const response = await fetch(`https://api.ratesapi.io/api/latest?base=${currency}`);
+    const response = await fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE}/${table}?api_key=${process.env.AIRTABLE_API_KEY}`)
     const data = await response.json();
     console.log(data);
     return data;
 }
 
 function Exchange(){
-    const [currency, setCurrency] = useState("SGD");
-    const {status, data, error} = useQuery(currency, fetchExchange, {refetchOnWindowFocus : false});
+    const [table, setTable] = useState("allRecord")
+    // const [currency, setCurrency] = useState("SGD");
+    const {status, data, error} = useQuery(table, fetchExchange, {refetchOnWindowFocus : false});
     if(status === "loading"){
         return <div>loading . . .</div>
     }
@@ -29,10 +31,10 @@ function Exchange(){
     }
     return (
       <div>
-          <button onClick={() => setCurrency("HKD")}>Hong Kong Dollar</button>
-          <button onClick={() => setCurrency("USD")}>US Dollar</button>
-          <button onClick={() => setCurrency("SGD")}>Singapore Dollar</button>
-    <h1>Showing currency {currency}</h1>
+          <button onClick={() => setTable("allRecord")}>Semua Data</button>
+          <button onClick={() => setTable("testData")}>Test Data</button>
+          <button onClick={() => setTable("Kabupaten")}>Kabupaten</button>
+    <h1>Showing currency {table}</h1>
 <pre>{JSON.stringify(data, null, 2)}</pre>
           </div>
     )
